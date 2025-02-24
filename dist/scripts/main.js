0920be3aa96bf341aa9f60850ef0f7ee5eea4995
lcheram <laracheramy@gmail.com>
2025-02-24 18:13:33 +0100

    remove htaccess + form / add js and commented it

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -4,3 +4,27 @@ window.addEventListener('scrollend', e => {
   $starfly.classList.toggle("active")
 });
 */
+/*
+const $hero = document.querySelector('.hero')
+const $section = document.querySelector('.content_section')
+const $h2 = document.querySelectorAll('h2')
+const $p = document.querySelectorAll('p')
+
+const $grid = document.querySelector('scroll_grid')
+
+/*visualViewport.addEventListener('scroll', e => {
+  $hero.classList.add('apparition')
+  $section.classList.add('apparition')
+})*/
+/*
+// ne fonctionne que sur la premier élément section
+onscroll = (event) => {
+  $hero.classList.add('apparition')
+  //$section.classList.add('apparition')
+};
+
+$grid.addEventListener('click', e => {
+  if($grid.contains('src')) {
+    //test des images en click agrandissement pop-up style galerie 
+  }
+})*/
